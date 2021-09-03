4830b7dd165ef4c327a0f53acb0e94927bde0a86
Kevin Willemet <kevin.willemet@contentsquare.com>
2021-09-03 12:09:52 +0200

    deleted bodyParser

--- README.md
+++ README.md
@@ -1,8 +1,31 @@
-- Implement:
+# Todo Server
+
+## Installation
+
+```bash
+# install dependencies
+yarn
+
+# serve with reload
+yarn start
+```
+
+## What needs to be done
+
+### Backend part
+
+The backend code is almost ready, the methods and routes needed are:
 
 - findById
 - updateTodo
 - delete
 
+### Frontend part
 
-Once done, implement a frontend using the framework of your choice to fit the backend
+Once done, implement the todolist UI with the framework of your choice.
+Here are different tasks to perform:
+
+- Listing the todos
+- Adding a todo
+- Updating a todo
+- Removing a todo
--- todolist.js
+++ todolist.js
@@ -3,7 +3,6 @@ const todo = require("./routes/todo");
 const http = require("http");
 const cors = require("cors");
 const morgan = require("morgan");
-const bodyParser = require("body-parser");
 const methodOverride = require("method-override");
 const errorHandler = require("errorhandler");
 const app = express();
@@ -11,18 +10,15 @@ const app = express();
 app.set("port", process.env.PORT || 3000);
 app.use(cors({ origin: "*" }));
 app.use(morgan("dev"));
-app.use(bodyParser.json());
+app.use(express.json());
 app.use(methodOverride());
-
 app.use(errorHandler());
 
 app.get("/api/todos", todo.findAll);
 app.post("/api/todos", todo.addTodo);
-
 app.delete("/api/todos/:id", todo.deleteTodo);
-
 // app.put('/api/todos/:id', todo.updateTodo);
-// app.get('/api/todos/:id', todo.findById);
+// app.get('/api/todos/:isd', todo.findById);
 
 http.createServer(app).listen(app.get("port"), function () {
   console.log("Express server listening on port " + app.get("port"));
